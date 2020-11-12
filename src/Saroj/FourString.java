1e149f8234877acb5bbaf4d4070b3a99e4633e2f
saroj kaul <sarojmahi73@gmail.com>
2020-11-12 14:14:12 +0100

    adding

---
+++ src/Saroj/FourString.java
@@ -0,0 +1,20 @@
+package Saroj;
+
+import java.util.Scanner;
+
+public class FourString {
+
+    public static void maskify(int input){
+
+
+    }
+
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number- ");
+        int a=sc.nextInt();
+        System.out.println(maskify(a));
+
+
+    }*/
+}
--- src/Saroj/Rent.java
+++ src/Saroj/Rent.java
@@ -1,5 +1,37 @@
 package Saroj;
 
-public class Rent {
+import java.util.Date;
 
+public class Rent {
+    Date Startdate;
+    Date endingdate;
+
+    public Rent(Date Startdate, Date endingdate){
+        this.Startdate = Startdate;
+        this.endingdate = endingdate;
+    }
+
+    public Date getEndingdate() {
+        return endingdate;
+    }
+
+    public void setEndingdate(Date endingdate) {
+        this.endingdate = endingdate;
+    }
+
+    public Date getStartdate() {
+        return Startdate;
+    }
+
+    public void setStartdate(Date startdate) {
+        Startdate = startdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Rent{" +
+                "Startdate=" + Startdate +
+                ", endingdate=" + endingdate +
+                '}';
+    }
 }
